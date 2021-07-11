54b18a22ceedd358f0de8aaa539ee8992fff2da7
Kevin Heinicke <kevin.heinicke@cern.ch>
2021-07-11 22:49:47 +0200

    Rename stuff

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,13 @@
 set -e # exit with nonzero exit code if anything fails
 
 # store password
-echo $PASSWORD > password-file
+echo $SSH_PRIV > id_rsa
+$(eval ssh-agent)
+ssh-add id_rsa
+rm id_rsa
 
 # compile the website
 # upload site
-rsync --password-file password-file -rq --links --delete --exclude=".*" \
+rsync -rq --links --delete --exclude=".*" \
   --exclude="Gemfile*" --exclude="deploy_key*" --exclude="password-file" . \
   $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
